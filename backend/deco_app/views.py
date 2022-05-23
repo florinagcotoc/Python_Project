0d184185a0d6809c1ea38fdb44c1d7be9dcf128c
florinagcotoc <florinacotoc@gmail.com>
2022-05-23 11:11:11 +0300

    Created views for products

--- backend/deco_app/views.py
+++ backend/deco_app/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from deco_app.models import Users,Products,Reviews,ShippingAddress,OrderItem,Orders
+from deco_app.serializers import UserCreateSerializer,ProductsSerializer,OrdersSerializer,OrderItemSerializer,ReviewsSerializer,ShippingAddressSerializer,ProductByCategorySerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from rest_framework import mixins, generics
 
-# Create your views here.
+User = get_user_model()
+
+class ProductsList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    
+    permission_classes = (AllowAny,)
+    
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+
+class ProductDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    
+    permission_classes = (AllowAny,)
+    
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def patch(self, request,pk):
+        return self.partial_update(request, pk)
+
+    def delete(self, request,pk):
+        return self.delete(request, pk)
+
+
+class ProductsByCategory(generics.ListAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = ProductByCategorySerializer
+    def get_queryset(self):
+        category = self.kwargs['category']
+        return Products.objects.filter(category=category)
+
