98696ac0e271abe911ed2810ba59224433e5e4c6
florinagcotoc <florinacotoc@gmail.com>
2022-05-23 11:03:17 +0300

    Included urls from deco_app and token-url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,13 +16,19 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView  #for react side
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView, TokenVerifyView
+
+
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('api/', include('deco_app.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
+
 
 
 urlpatterns += [
